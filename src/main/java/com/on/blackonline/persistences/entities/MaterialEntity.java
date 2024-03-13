e1fcc5a70bb03c84a764505d5e3a8c453eae78c3
Juan Buenrostro <juanmbv.31@gmail.com>
2024-03-13 07:35:06 -0600

    Fix relation many to one in material entity

--- src/main/java/com/on/blackonline/persistences/entities/MaterialEntity.java
+++ src/main/java/com/on/blackonline/persistences/entities/MaterialEntity.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -19,6 +21,10 @@ public class MaterialEntity {
 
 	@Column(name = "price")
 	private int price;
+
+	@ManyToOne
+	@JoinColumn(name = "material_purchase_id")
+	private MaterialPurchaseEntity materialPurchase;
 
 	// Falta la relación muchos a muchos con la tabla de tecnicas
 }
