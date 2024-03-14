c6f9b2a2f1c04da32c464621f159a469e175e75a
Juan Buenrostro <juanmbv.31@gmail.com>
2024-03-13 20:20:33 -0600

    Add swagger

--- src/main/java/com/on/blackonline/BlackonlineApplication.java
+++ src/main/java/com/on/blackonline/BlackonlineApplication.java
@@ -2,6 +2,10 @@ package com.on.blackonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class BlackonlineApplication {
@@ -10,4 +14,12 @@ public class BlackonlineApplication {
 		SpringApplication.run(BlackonlineApplication.class, args);
 	}
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Blackonline API")
+                        .version("1.0")
+                        .description("Documentation Blackonline API v1.0"));
+    }
 }
